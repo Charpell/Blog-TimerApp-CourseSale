017c986dc0b0a536df40388817488c20b7a48f68
Charpell <charpellumeh@gmail.com>
2018-04-17 09:43:18 +0100

    Break down app into different Components: Header, Footer, Navbar, Posts, OlderPost

---
+++ src/OlderPost.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class OlderPost extends Component {
+  render() {
+    return (
+      <div className="clearfix">
+        <a className="btn btn-primary float-right" href="#">
+          Older Posts &rarr;
+        </a>
+      </div>
+    );
+  }
+}
+
+export default OlderPost;
