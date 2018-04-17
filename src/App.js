f7b886b6e8a8347d839b8e0ec7b178bbb75f4dc0
Charpell <charpellumeh@gmail.com>
2018-04-17 11:38:46 +0100

    Clean App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import Timer from "./Timer";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Timer start={Date.now()} />
+        
       </div>
     );
   }
