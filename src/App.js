0e5c35fbd5d0187b4af29cd38c04c6a57ffd2b08
Charpell <charpellumeh@gmail.com>
2018-04-17 09:50:27 +0100

    Setup Timer Project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, { Component } from "react";
-import Navbar from "./Blog/Navbar";
-import Header from "./Blog/Header";
-import Footer from "./Blog/Footer";
-import Posts from "./Blog/Posts";
-import OlderPost from "./Blog/OlderPost";
+import Timer from "./Timer";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <Header />
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8 col-md-10 mx-auto">
-              <Posts />
-              <hr />
-              <OlderPost />
-            </div>
-          </div>
-        </div>
-        <hr />
-        <Footer />
+        <Timer />
       </div>
     );
   }
---
+++ src/Timer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class Timer extends Component {
+  render() {
+    return(
+      <div>
+        I am a timer
+      </div>
+    )
+  }
+}
+
+export default Timer;
