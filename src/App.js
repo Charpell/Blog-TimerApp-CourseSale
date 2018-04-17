adbc932e07cf468c0b79ef0a6b0a8591e4959134
Charpell <charpellumeh@gmail.com>
2018-04-17 12:02:06 +0100

    Finish Coursesale project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+
+import Coursesales from './CourseSales'
 import './App.css';
 
 class App extends Component {
   render() {
+    var courses = [
+      { name: 'Complete iOS10 dev course', price: 199},
+      { name: 'Complete presenting course', price: 299},
+      { name: 'Complete front end dev course', price: 180},
+      { name: 'Big Boujnty and web app presenting', price: 190},
+    ]
     return (
       <div className="container">
-        
+        <h2>Welcome to Course Purchase Page</h2>
+        <Coursesales items={courses} />
       </div>
     );
   }
